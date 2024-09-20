ca37b52da77648a9a11b54f27dfa91c8115d677a
yhs <yhsdeveloper8746@gmail.com>
2024-09-20 17:04:45 +0900

    9월 20일 커밋
    React Quill Editor Ref undefined문제 해결 필요

--- next-blog/src/app/posts/new/components/QuillEditor/QuillEditor.tsx
+++ next-blog/src/app/posts/new/components/QuillEditor/QuillEditor.tsx
@@ -1,12 +1,22 @@
 import dynamic from "next/dynamic";
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import React, {
+  forwardRef,
+  MutableRefObject,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import DOMPurify from "dompurify";
 
 // import "react-quill/dist/quill.bubble.css"; // Bubble 테마의 CSS 파일
 
 import "react-quill/dist/quill.snow.css"; // Snow 테마 CSS 파일
 
-const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
+// Import the Quill editor dynamically and handle the ref forwarding explicitly
+const ReactQuill: any = dynamic(() => import("react-quill"), {
+  ssr: false,
+});
 
 interface QuillEditorProps {
   value: string;
@@ -14,102 +24,116 @@ interface QuillEditorProps {
 }
 
 //React.memo를 통해 props값이 변하지 않으면 QuillEditor의 재렌더링을 막는다. 따라서 BlogForm에서 제목의 내용을 변경해도 QuillEditor에는 제목 관련 Props가 없기 때문에 재렌더링 되지 않는다.
-export default React.memo(function QuillEditor({
-  value,
-  onChange,
-}: QuillEditorProps) {
-  const quillRef = useRef<any>(null); // Quill 인스턴스를 참조하는 useRef
+export default React.memo(
+  forwardRef(function QuillEditor({ value, onChange }: QuillEditorProps, ref) {
+    const quillRef: any = useRef(null);
 
-  console.log("실행행행해 외부앻ㅇ");
-  // 여기서 함수 재생성을 막아야 올바르게 작동함. 
-  const handleFileSelection = useCallback(() => {
-    console.log("실행행행해앻ㅇ");
+ 
 
-    const input = document.createElement("input");
-    input.setAttribute("type", "file");
-    input.setAttribute("accept", "*/*");
-    input.click();
+    // 여기서 함수 재생성을 막아야 올바르게 작동함.
+    const handleFileSelection = useCallback(() => {
+      const input = document.createElement("input");
+      input.setAttribute("type", "file");
+      input.setAttribute("accept", "*/*");
 
-    input.onchange = async () => {
-      const file = input.files ? input.files[0] : null;
-      if (file) {
-        const reader = new FileReader();
-        reader.onload = () => {
-          const base64String = reader.result?.toString();
-          const quill = quillRef.current?.getEditor(); // Quill 인스턴스 안전하게 참조
-          if (quill && base64String) {
-            const fileType = file.type.includes("image") ? "image" : "file";
-            if (fileType === "image") {
-              quill.insertEmbed(
-                quill.getSelection()?.index || 0,
-                "image",
-                base64String
-              );
-            } else {
-              quill.insertText(
-                quill.getSelection()?.index || 0,
-                `${file.name} 첨부됨`, // 문자열 리터럴로 수정
-                "file",
-                base64String
-              );
+      console.log("input >>>", input);
+      input.click();
+
+      input.onchange = () => {
+        const file = input.files ? input.files[0] : null;
+
+        if (file) {
+          const reader = new FileReader();
+          reader.onload = async () => {
+            const base64String = reader.result?.toString();
+            // const quill = (document.querySelector(".ql-editor") as any).__blot;
+            const quill = quillRef.current?.getEditor();
+
+            console.log("quill >>> ", quill);
+
+            if (quill && base64String) {
+              const fileType = file.type.includes("image") ? "image" : "file";
+
+              console.log("filetype >>>" + fileType);
+
+              if (fileType === "image") {
+                await quill.insertEmbed(
+                  quill.getSelection()?.index || 0,
+                  "image",
+                  base64String
+                );
+              } else {
+                await quill.insertText(
+                  quill.getSelection()?.index || 0,
+                  `${file.name} 첨부됨`, // 문자열 리터럴로 수정
+                  "file",
+                  base64String
+                );
+              }
             }
-          }
-        };
-        reader.readAsDataURL(file);
-      }
-    };
-  }, []);
+          };
+          reader.readAsDataURL(file);
+        }
+      };
+    }, []);
 
-  // [{"header": [1, 2, 3, 4, 5, 6, false]}],
-  // ["bold", "italic", "underline"],
-  // [{ "list": "ordered" }, { "list": "bullet" }],
-  // ["link", "image"],
-  // [{ "align": [] }],
-  // [{ 'color': [] }, { 'background': [] }],
-  // ["clean"]
-  // ],
+    // [{"header": [1, 2, 3, 4, 5, 6, false]}],
+    // ["bold", "italic", "underline"],
+    // [{ "list": "ordered" }, { "list": "bullet" }],
+    // ["link", "image"],
+    // [{ "align": [] }],
+    // [{ 'color': [] }, { 'background': [] }],
+    // ["clean"]
+    // ],
 
-  const modules = {
-    toolbar: {
-      container: [
-        [{ header: [1, 2, 3, 4, 5, 6, false] }],
-        ["bold", "italic", "underline"],
-        [{ list: "ordered" }, { list: "bullet" }],
-        ["link", "image"],
-        [{ align: [] }],
-        [{ color: [] }, { background: [] }],
-        ["clean"],
-      ],
-      handlers: {
-        image: handleFileSelection, // 이미지 핸들러 적용
+    const modules = {
+      toolbar: {
+        container: [
+          [{ header: [1, 2, 3, 4, 5, 6, false] }],
+          ["bold", "italic", "underline"],
+          [{ list: "ordered" }, { list: "bullet" }],
+          ["link", "image"],
+          [{ align: [] }],
+          [{ color: [] }, { background: [] }],
+          ["clean"],
+        ],
+        handlers: {
+          image: handleFileSelection, // 이미지 핸들러 적용
+        },
       },
-    },
-  };
+    };
 
-  const handleChange = (html: string) => {
-    const sanitizedHtml = DOMPurify.sanitize(html); // HTML을 안전하게 정리
-    // const parser = new DOMParser();
-    // const doc = parser.parseFromString(sanitizedHtml, "text/html");
-    // const textContent = doc.body.textContent || ""; // 텍스트만 추출
-    // onChange(textContent);
-    onChange(sanitizedHtml);
-  };
+    const handleChange = (html: string) => {
+      const sanitizedHtml = DOMPurify.sanitize(html); // HTML을 안전하게 정리
+      // const parser = new DOMParser();
+      // const doc = parser.parseFromString(sanitizedHtml, "text/html");
+      // const textContent = doc.body.textContent || ""; // 텍스트만 추출
+      // onChange(textContent);
+      onChange(sanitizedHtml);
+    };
 
-  useEffect(() => {
-    if (quillRef.current) {
-      console.log("ㅊ기화");
-    }
-  });
+    useEffect(() => {
+      // quillRef.current가 정의된 후에만 getEditor를 호출
+      const checkQuillReady = setTimeout(() => {
+        if (quillRef.current) {
+          const editor = quillRef.current.getEditor();
+          console.log("Quill Editor 로드 완료", editor);
+        }
+      }, 500); // 500ms 후에 quill이 로드되었는지 체크
 
-  return (
-    <>
-      <ReactQuill
-        value={value}
-        onChange={handleChange}
-        theme="snow"
-        modules={modules}
-      />
-      {/* <ReactQuill value={value} onChange={handleChange} theme='bubble' /> */}
-    </>
-  );
-});
+      return () => clearTimeout(checkQuillReady);
+    }, []);
+    return (
+      <>
+        <ReactQuill
+          ref={quillRef}
+          value={value}
+          onChange={handleChange}
+          theme="snow"
+          modules={modules}
+        />
+        {/* <ReactQuill value={value} onChange={handleChange} theme='bubble' /> */}
+      </>
+    );
+  })
+);
