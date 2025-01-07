75f484c2cd3b2adea2fd8439473dc3d0ccf72165
yhs <yhsdeveloper8746@gmail.com>
2025-01-08 05:11:06 +0900

    chore: 주석 처리 추가 - 코드 보존용 커밋

--- next-blog/src/app/[blogId]/components/AuthCheck.tsx
+++ next-blog/src/app/[blogId]/components/AuthCheck.tsx
@@ -4,7 +4,7 @@ import { useParams } from "next/navigation";
 
 import { useEffect, useRef, useState } from "react";
 
-import { useAuthStore, userPrivateProfileStore } from "@/store/appStore";
+import { useAuthStore } from "@/store/appStore";
 
 import GlobalLoading from "@/app/loading";
 
@@ -13,27 +13,27 @@ export default function AuthCheck({ children }: { children: React.ReactNode }) {
     const params = useParams();
     const pathBlogId = params.blogId as string;
     const { isHeaderLogin, isAuthenticated, isInitialized } = useAuthStore();
-    const { privateBlogId } = userPrivateProfileStore();
+    // const { privateBlogId } = userPrivateProfileStore();
 
     // 비로그인 사용자, 로그인 사용자 모두 검증
-    useEffect(() => {
-        const accessToken = localStorage.getItem("access_token") ?? false;
-        if (!accessToken) {
-            throw new Error("접근 권한이 없습니다.");
-        }
+    // useEffect(() => {
+    //     const accessToken = localStorage.getItem("access_token") ?? false;
+    //     if (!accessToken) {
+    //         throw new Error("접근 권한이 없습니다.");
+    //     }
 
-        if (isHeaderLogin || isInitialized || isAuthenticated) {
+    //     if (isHeaderLogin || isInitialized || isAuthenticated) {
 
-            console.log("privateBlogId: ", privateBlogId);
-            console.log("pathBlogId: ", pathBlogId);
+    //         console.log("privateBlogId: ", privateBlogId);
+    //         console.log("pathBlogId: ", pathBlogId);
 
-            if (privateBlogId !== pathBlogId) {
-                throw new Error("접근 권한이 없습니다.");
-            }
+    //         if (privateBlogId !== pathBlogId) {
+    //             throw new Error("접근 권한이 없습니다.");
+    //         }
 
-            setIsChecking(false);
-        }
-    }, [isHeaderLogin, pathBlogId, isAuthenticated, isInitialized]);
+    //         setIsChecking(false);
+    //     }
+    // }, [isHeaderLogin, pathBlogId, isAuthenticated, isInitialized]);
 
     if (isChecking) {
         return <GlobalLoading type='auth' message='사용자 인증 진행 중...' />;
