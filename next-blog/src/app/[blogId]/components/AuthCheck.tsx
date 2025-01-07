3b8494fe66c725ab655a696bc31979ba6c0dd4d7
yhs <yhsdeveloper8746@gmail.com>
2025-01-08 05:15:24 +0900

    chore: AuthCheck jwt decode 부분 원상 복구

--- next-blog/src/app/[blogId]/components/AuthCheck.tsx
+++ next-blog/src/app/[blogId]/components/AuthCheck.tsx
@@ -8,32 +8,39 @@ import { useAuthStore } from "@/store/appStore";
 
 import GlobalLoading from "@/app/loading";
 
+import { jwtDecode } from "jwt-decode";
+
+interface JwtPayload {
+    blogId: string;
+}
+
 export default function AuthCheck({ children }: { children: React.ReactNode }) {
     const [isChecking, setIsChecking] = useState<boolean>(true);
     const params = useParams();
     const pathBlogId = params.blogId as string;
     const { isHeaderLogin, isAuthenticated, isInitialized } = useAuthStore();
-    // const { privateBlogId } = userPrivateProfileStore();
 
-    // 비로그인 사용자, 로그인 사용자 모두 검증
-    // useEffect(() => {
-    //     const accessToken = localStorage.getItem("access_token") ?? false;
-    //     if (!accessToken) {
-    //         throw new Error("접근 권한이 없습니다.");
-    //     }
+    useEffect(() => {
+        const accessToken = localStorage.getItem("access_token") ?? false;
+        if (!accessToken) {
+            throw new Error("접근 권한이 없습니다.");
+        }
 
-    //     if (isHeaderLogin || isInitialized || isAuthenticated) {
+        if (isHeaderLogin || isInitialized || isAuthenticated) {
+            console.log("isHeaderLogin isHeaderLogin>>>", isHeaderLogin);
 
-    //         console.log("privateBlogId: ", privateBlogId);
-    //         console.log("pathBlogId: ", pathBlogId);
+            try {
+                const decoded = jwtDecode<JwtPayload>(accessToken);
+                if (decoded.blogId !== pathBlogId) {
+                    throw new Error("접근 권한이 없습니다.");
+                }
 
-    //         if (privateBlogId !== pathBlogId) {
-    //             throw new Error("접근 권한이 없습니다.");
-    //         }
-
-    //         setIsChecking(false);
-    //     }
-    // }, [isHeaderLogin, pathBlogId, isAuthenticated, isInitialized]);
+                setIsChecking(false);
+            } catch (error) {
+                throw new Error("접근 권한이 없습니다.");
+            }
+        }
+    }, [isHeaderLogin, pathBlogId, isAuthenticated, isInitialized]);
 
     if (isChecking) {
         return <GlobalLoading type='auth' message='사용자 인증 진행 중...' />;
