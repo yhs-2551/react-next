57d16a89c62165d061caeb73120ac5dc62ff9d95
yhs <yhsdeveloper8746@gmail.com>
2025-04-11 01:13:21 +0900

    로딩중 메시지 제거

--- next-blog/src/app/loading.tsx
+++ next-blog/src/app/loading.tsx
@@ -5,12 +5,12 @@ interface GlobalLoadingProps {
 
 import { ClipLoader } from "react-spinners";
 
-export default function GlobalLoading({ type = "default", message = "로딩 중..." }: GlobalLoadingProps) {
+export default function GlobalLoading({ type = "default", message }: GlobalLoadingProps) {
     return (
         <div className='fixed inset-0 flex items-center justify-center bg-slate-50/95 backdrop-blur-sm z-[9999]'>
             <div className='text-center'>
                 <ClipLoader color='#1e293b' size={40} className='mb-3' />
-                <p className='text-gray-800 text-lg font-medium'>{message}</p>
+                {message && <p className='text-gray-800 text-lg font-medium'>{message}</p>}
             </div>
         </div>
     );
